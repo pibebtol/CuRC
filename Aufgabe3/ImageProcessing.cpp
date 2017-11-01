b7a49a2ff892392d52ce6c7c4511fa00d7d69bd9
Peter Klausing <peter_klausing@wh2.tu-dresden.de>
2017-11-01 16:59:55 +0100

    Aufgabe3: add some filters

--- Aufgabe3/ImageProcessing.cpp
+++ Aufgabe3/ImageProcessing.cpp
@@ -7,15 +7,53 @@ using namespace cv;
 int main(int argc, char** argv )
 {
 
-    if ( argc < 3 )
+    if ( argc < 3)
     {
-        printf("usage: ImageProcessing <image> <output image>\n");
+        printf("usage: ImageProcessing <image> <output>\n");
         return -1;
     }
 
-    cv::Mat image = imread(argv[1]); //Load image as grayscale
+    Size size(3,3);
 
-    //Process images
-    
-    imwrite(argv[2], image);
+    String output_fn = String(argv[2]);
+    int i = 0;
+	Mat image_filtered;
+	//Load image 
+    Mat image = imread(argv[1],  IMREAD_COLOR);
+	
+	//Write original
+    printf("Process original\n");
+	imwrite(output_fn + String(i++) + "_orig.png", image);
+	
+	// Grayscale
+    printf("Process Grayscale\n");
+	cvtColor(image, image_filtered, cv::COLOR_RGB2GRAY);
+    imwrite(output_fn + String(i++) + "_grayscale.png", image_filtered);
+
+	//Rechteckfilter
+    printf("Process Rechteckfilter\n");
+	blur(image, image_filtered, size);
+    imwrite(output_fn + "_blur.png", image_filtered);
+	
+	//Gausian filter
+    printf("Process Gausian filter\n");
+	double sigma = 1;
+	GaussianBlur(image, image_filtered, size, sigma);
+    imwrite(output_fn + "_gausian.png", image_filtered);
+
+	//Median
+    printf("Process median Blur\n");
+	medianBlur(image, image_filtered, 3);
+    imwrite(output_fn + "_median.png", image_filtered);
+	
+	//TODO
+	/*Kantenerkennung
+    printf("Process median Blur\n");
+	 */
+	
+	//Sobel
+	
+	//Laplace
+	
+	// Laplace on Gausian
 } 
