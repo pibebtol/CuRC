659dc07e2130a64f472852e6d2f904c4a074723c
Peter Klausing <peter_klausing@wh2.tu-dresden.de>
2018-01-12 16:49:13 +0100

    Aufgabe9*: initial commit

---
+++ Aufgabe9/RecInstruments.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char** argv )
+{
+  if ( argc < 2)
+    {
+      cout << "usage: RecInstruments <sample_folder>" << endl;
+      return -1;
+    }
+
+}
+
